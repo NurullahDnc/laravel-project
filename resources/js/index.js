b971ba220b7a30c1fa762074dd224d5c05e2438f
NurullahDnc <150585098+NurullahDnc@users.noreply.github.com>
2023-12-28 03:05:55 +0300

    proje olusturuldu kurulumla yapıldı

---
+++ resources/js/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'));
+
